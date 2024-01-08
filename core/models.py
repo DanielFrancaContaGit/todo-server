df18883686aa2c512459401d11636f7907e4f305
Daniel <daniel.pe.15y15@gmail.com>
2024-01-07 21:13:16 -0300

    add finished fild to todo

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Todo(models.Model):
     objects=models.Manager
     content = models.CharField(max_length=400)
+    finished = models.BooleanField(default=False)
 
     def __str__(self):
         return f'conteudo do todo: {self.content}'
